10149b19decaf6a475dfd8d4e1d5818939192a0d
veeru.mot <veerender.veeru2009@gmail.com>
2025-07-06 15:19:51 -0400

    corrected the path for etag s3

--- Infra/modules/S3/s3.tf
+++ Infra/modules/S3/s3.tf
@@ -19,8 +19,8 @@ resource "aws_s3_bucket_acl" "bot-logs-data" {
 resource "aws_s3_object" "data" {
   bucket = aws_s3_bucket.bot-logs-data.bucket
   key    = "bot_data.json"  # Replace with your desired object key
-  source = "/Users/madhurigorantla/github/Lambda-Hackthon/bot/bot_data.json"  # Replace with the path to your local file
-  etag   = filemd5("/Users/madhurigorantla/github/Lambda-Hackthon/bot/bot_data.json") # Optional: ETag for object versioning
+  source = "/Users/madhurigorantla/github/Lambda-Hackathon/bot/bot_data.json"  # Replace with the path to your local file
+  etag   = filemd5("/Users/madhurigorantla/github/Lambda-Hackathon/bot/bot_data.json") # Optional: ETag for object versioning
 }
 
 output "aws_s3_bucket_bot-logs-data_arn"{
